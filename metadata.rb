8e2adae49233383cdec25e607d5739011c93d6ea
Brett Petrusek <pretusek@us.ibm.com>
2016-05-31 10:57:22 -0400

    initial fork of libzabbix. updated method calls to pluralize class names

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bill.warner@gmail.com'
 license 'Apache 2.0'
 description 'Adds Zabbix Server related LWRPs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 supports 'ubuntu', '>= 10.04'
 supports 'debian', '>= 6.0'
 supports 'redhat', '>= 5.0'
@@ -12,3 +12,5 @@ supports 'centos', '>= 5.0'
 supports 'oracle', '>= 5.0'
 
 recommends 'zabbix-agent'
+
+depends 'zabbix'
